8aacabcde5a4596d2389fc48a6040714bd068671
Ospina <o.lamprea99@gmail.com>
2023-09-08 19:49:33 -0500

    Archivo Sistema creado

---
+++ CSistema.py
@@ -0,0 +1,12 @@
+
+class Sistema:
+    def __init__(self):
+        self.juego_nuevo = False
+
+    def registrar_jugador(self):
+        pass
+    def inicar_juego(self):
+        pass
+    def anunciar_resultado(self):
+        pass
+
